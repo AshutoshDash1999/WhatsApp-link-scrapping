f93b1bc0c7317bdaf6e0e775805b2bf0450a25dd
AshutoshDash1999 <ashu.vampire13@gmail.com>
2020-05-17 21:06:16 +0530

    code update

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-This repo is for scrapping link from whatsapp contact.
+#WhatsApp-link-scrapper
 
-Install geckodriver for firefox and chromedriver for chrome.
+This repo is for scrapping link from WhatsApp contact.
+
+Make sure you have good internet connection.
+
+Install `geckodriver` for Firefox and `chromedriver` for Google Chrome.
+
+First install [Homebrew](https://brew.sh/) for Linux/Mac, then on terminal write `brew install geckodriver` / `brew install chromedriver`.
+
+Install [python](https://www.python.org/downloads/release), [selenium](https://selenium-python.readthedocs.io/installation.html)
 
 1. Run Script
 2. Scan QR code
-3. Enter the person's name whose links you want to scrap.
+3. Wait for the browser to get to the `web.whatsapp.com`.
+4. Enter the person's name whose links you want to scrap.
+5. Press `Enter`
--- whatsapp-link-scrapping.py
+++ whatsapp-link-scrapping.py
@@ -1,20 +1,40 @@
 # requirements:- selenium, geckodriver(firefox)/chromedriver(chrome),
 
-from selenium import webdriver as wd
+from selenium import webdriver as wd  # import library
 
 print("Scan the QR code then come here.")
-driver = wd.Firefox()
-driver.get("https://web.whatsapp.com/")
+browser = input("Enter your fav. browser(Firefox/Chrome): ")
+if browser == "Firefox":
+    driver = wd.Firefox()  # open firefox
+elif browser == "Chrome":
+    driver = wd.Chrome()
+else:
+    print("Choose between Firefox/Chrome.")
+
+driver.get("https://web.whatsapp.com/")     # redirect to site
 input("Press 'ENTER' for the next step.")
-name = input("Enter the contact name here: ")
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+
+name = input("Enter the contact name here: ")   # enter the name whose links you want to  access
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))  # access the path to contacts chat
 user.click()
 
 user_profile = user.find_element_by_xpath('//header[@class="_2y17h"]')
-user_profile.click()
+user_profile.click()    # click on the profile
 
-user_media_menu = user_profile.find_element_by
-user_media_menu.click()
+user_media_menu = user_profile.find_element_by_xpath('//span[contains(text(), "Media, Links and Docs")]')
+user_media_menu.click()  # click on the "Media,docs and links"
 
-link = user_media_menu.find_element_by_link_text("Links")
-link.click()
+link_bar = user_media_menu.find_element_by_xpath('//button[@title="Links"]')    # click on "Links" tab
+link_bar.click()
+
+links = []
+elements = driver.find_elements_by_tag_name('a')
+for elem in elements:
+    href = elem.get_attribute('href')
+    links.append(href)
+print(links)
+
+# writing the links list to a file
+with open("whatsapp_links.txt", 'w') as f:
+    for link in links:
+        f.write(link)
