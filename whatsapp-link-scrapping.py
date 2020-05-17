356d00d51388803fbb64dbfd3a10096aed5dd16b
AshutoshDash1999 <ashu.vampire13@gmail.com>
2020-05-17 09:12:26 +0530

    added comments

--- whatsapp-link-scrapping.py
+++ whatsapp-link-scrapping.py
@@ -1,20 +1,20 @@
 # requirements:- selenium, geckodriver(firefox)/chromedriver(chrome),
 
-from selenium import webdriver as wd
+from selenium import webdriver as wd  # import library
 
 print("Scan the QR code then come here.")
-driver = wd.Firefox()
-driver.get("https://web.whatsapp.com/")
+driver = wd.Firefox()  # open firefox
+driver.get("https://web.whatsapp.com/")     # redirect to site
 input("Press 'ENTER' for the next step.")
-name = input("Enter the contact name here: ")
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+name = input("Enter the contact name here: ")   # enter the name whose links you want to  access
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))  # access the path to contacts chat
 user.click()
 
 user_profile = user.find_element_by_xpath('//header[@class="_2y17h"]')
-user_profile.click()
+user_profile.click()    # click on the profile
 
-user_media_menu = user_profile.find_element_by
+user_media_menu = user_profile.find_element_by      # under construction
 user_media_menu.click()
 
-link = user_media_menu.find_element_by_link_text("Links")
+link = user_media_menu.find_element_by_link_text("Links")    #under construction
 link.click()
