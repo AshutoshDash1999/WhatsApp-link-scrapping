b014f338b99f7a9ba07e5d19bdf934f24e4b1e40
Ashutosh Dash <ashu.vampire13@gmail.com>
2020-05-18 11:00:06 +0530

    Update whatsapp-link-scrapping.py

--- whatsapp-link-scrapping.py
+++ whatsapp-link-scrapping.py
@@ -1,14 +1,25 @@
 # requirements:- selenium, geckodriver(firefox)/chromedriver(chrome),
-
 from selenium import webdriver as wd  # import library
 
 print("Scan the QR code then come here.")
-driver = wd.Firefox()  # open firefox
+browser = input("Enter your fav. browser(Firefox/Chrome): ")
+if browser == "Firefox":
+    driver = wd.Firefox()  # open firefox
+elif browser == "Chrome":
+    driver = wd.Chrome()
+else:
+    print("Choose between Firefox/Chrome.")
+
 driver.get("https://web.whatsapp.com/")     # redirect to site
-input("Press 'ENTER' for the next step.")
-name = input("Enter the contact name here: ")   # enter the name whose links you want to  access
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))  # access the path to contacts chat
-user.click()
+
+print("Please wait while the site is being loaded.")
+
+try:
+    name = input("Enter the contact name here: ")   # enter the name whose links you want to  access
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))  # access the path to contacts chat
+    user.click()
+except Exception as e:
+    print("Please enter the correct name.\nThe name is character specific.\nError:", e)
 
 user_profile = user.find_element_by_xpath('//header[@class="_2y17h"]')
 user_profile.click()    # click on the profile
@@ -19,10 +30,18 @@ user_media_menu.click()  # click on the "Media,docs and links"
 link_bar = user_media_menu.find_element_by_xpath('//button[@title="Links"]')    # click on "Links" tab
 link_bar.click()
 
+input("Please wait for the page to load completely and then press 'ENTER'.")
 
-links = []
+links = list(set([]))  # coverting to set as we dont require repeated links
 elements = driver.find_elements_by_tag_name('a')
 for elem in elements:
     href = elem.get_attribute('href')
     links.append(href)
-print(links)
+
+
+# writing the links list to a file
+with open("whatsapp_links.txt", 'w') as f:
+    for link in links:
+        f.write(link)
+        f.write('\n')
+f.close()
